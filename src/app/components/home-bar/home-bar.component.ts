4cf4b7c085cff6786bbbb498fd78105e73e29093
Damien Nevaldo <damien.nevaldo@gmail.com>
2020-10-22 12:29:47 +0200

    Last run en rouge si différence supérieure à 24 heures

--- src/app/components/home-bar/home-bar.component.ts
+++ src/app/components/home-bar/home-bar.component.ts
@@ -1,6 +1,8 @@
 import { ApiCallsService } from './../../services/api-calls/api-calls.service';
 import { Component, OnInit } from '@angular/core';
 
+const MS_IN_A_DAY = 86400000;
+
 @Component({
   selector: 'app-home-bar',
   templateUrl: './home-bar.component.html',
@@ -10,21 +12,29 @@ export class HomeBarComponent implements OnInit {
 
   public now: Date = new Date();
   public lastRunDate: Date;
+  public isMoreThanADay = false;
 
   constructor(private apiCalls: ApiCallsService) {
     setInterval(() => {
       this.now = new Date();
+      if (this.lastRunDate){
+        this.isMoreThanADay = (this.now.getTime() - this.lastRunDate.getTime() > MS_IN_A_DAY);
+      }
     }, 1);
   }
 
   async ngOnInit(): Promise<void> {
     const promise$ =  await this.apiCalls.getLastRun();
-    console.log('promise$');
-    console.dir(promise$);
     const data = promise$.data;
+    console.dir(promise$);
     if (data && data.Code === 0 && data.Data) {
-      this.lastRunDate= data.Data.InsertedDate;
+      const date = new Date(data.Data.InsertedDate);
+      const timeZoneDifference = (date.getTimezoneOffset() / 60) * -1; // convert to positive value.
+      date.setTime(date.getTime() - (timeZoneDifference * 60) * 60 * 1000);
+      this.lastRunDate = date;
     }
+    console.log(this.now);
+    console.log(this.lastRunDate);
   }
 
   last_run(): string {
--- src/app/components/models-score/models-score.component.ts
+++ src/app/components/models-score/models-score.component.ts
@@ -15,22 +15,13 @@ export class ModelsScoreComponent implements OnInit {
 
   async ngOnInit(): Promise<void> {
     const retour =  await this.apiCalls.getScore(20);
-    console.dir(retour);
     const data = retour.data;
     if (data && data.Code === 0 && data.Data) {
       const newMap = new Map();
       newMap.set('score', '');
       const returnMap: Map<number, number> = data.Data;
-      console.dir(returnMap);
       const array = new Array().concat(returnMap);
       this.data = array;
-      /*console.dir(returnMap.keys());
-      for (let key of returnMap.keys()) {
-        const value = returnMap.get(key);
-        newMap.set(key, value);
-      }
-      const array = new Array().concat(newMap);
-      console.dir(array);*/
     }
   }
 
